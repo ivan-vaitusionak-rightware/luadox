97ba903d10aab269e42e41b5bac07cd8354b585b
Ivan Vaitusionak <ivan.vaitusionak@rightware.com>
2026-09-01 08:05:28 +0200

    Review: surface the deprecated flag in json and yaml

--- luadox/render/json.py
+++ luadox/render/json.py
@@ -97,6 +97,8 @@ class JSONRenderer(Renderer):
             'symbol': colref.symbol,
             'heading': colref.heading,
         }
+        if 'deprecated' in colref.flags:
+            section['deprecated'] = colref.flags['deprecated'] or True
         section.update({k:v for k, v in kwargs.items() if v})
         content = self._render_content(colref.content)
         if content:
@@ -177,6 +179,9 @@ class JSONRenderer(Renderer):
             field['types'] = self._render_types(ref.types)
         if ref.meta:
             field['meta'] = ref.meta
+        if 'deprecated' in ref.flags:
+            # Presence signals deprecation; the value is the explanation (or true when bare).
+            field['deprecated'] = ref.flags['deprecated'] or True
         content = self._render_content(ref.content)
         if content:
             field['content'] = content
